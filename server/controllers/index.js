5dfc08c32bd4bac8433935a2ad585d4aa9064825
William Henshaw <whenshaw87@googlemail.com>
2015-12-04 18:23:28 -0800

    Got server sending and recieving data

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,8 +3,21 @@ var models = require('../models/index.js'); //require models
 module.exports = {
   signin: {
     post: function (req, res) {
-      params = "blah"; //this is temporary
+      console.log("req.body....",req)
+      console.log("req.body222....",res)
+      params = [req.body.username, req.body.password]; //this is temporary
+      console.log("params....", params)
       models.users.signin(params, function(results) {
+        res.status(201).send(results);// sends status of 201 and results of get request to client
+      });
+    }
+  },
+  signup: {
+    post: function (req, res) {
+      console.log("username....", req.body);
+      params = [req.body.username, req.body.firstname, req.body.secondname, req.body.password]; //this is temporary
+      models.users.signup(params, function(results) {
+        console.log(results);
         res.status(201).send(results);// sends status of 201 and results of get request to client
       });
     }
--- server/models/index.js
+++ server/models/index.js
@@ -5,16 +5,39 @@ module.exports = {
   users: {
      signin: function (params, callback) {
       console.log("in here")
-      db('users').insert([{username: 'Dyn'}, {firstname: "Liam"}, {secondname: "Gallagher"}, {hashedpw: crypt("blue", gen_salt("md5"))}]).then(function(ret){
-          console.log("success")
-        });
-      db.select().table('users').then(function (result) {
-        console.log("in here....", result)
-        callback(err, result);
-      }).catch(function (error){
-        console.log(error)
-      });
+      // db('users').insert([
+      //   {username: 'wow'}, 
+      //   {firstname: "Liam"}, {secondname: "Gallagher"}, 
+      //   {hashedpw: db.raw( "crypt('blue', gen_salt('md5'))" )}
+      //   ]).then(function(ret){
+      //     console.log("success")
+      //   });
+      // db('users').where('username', 'wo')
+      // .then(function (res) {
+      //   console.log('result', res)
+      // })
+      db('users').
+      where( db.raw("Upper('username')"), 'like', '%' + params[0] + '%').
+      where({
+        hashedpw: db.raw( "crypt('"+ params[1] + "', hashedpw)")
+      }).select()
+      .then(function (res) {
+        console.log(res);
+        callback(res)
+      })
+      .catch(function (error){
+      callback(error)
+    });
     
+      },
+      signup: function (params, callback) {
+        db('users').insert( {username: params[0],
+          firstname: params[1],
+          secondname: params[2],
+          hashedpw: db.raw( "crypt('"+ params[3] + "', gen_salt('md5'))" )}).then(function(ret){
+          console.log("success")
+          callback(ret);
+        });
       }
     }
 
