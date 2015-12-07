3a928bd269db846bfb269357c346a97db86108e9
William Henshaw <whenshaw87@googlemail.com>
2015-12-07 10:39:09 -0800

    dded server changes

--- client/components/signup/index.js
+++ client/components/signup/index.js
@@ -1,14 +1,31 @@
 import React from "react";
 
+import Request from "superagent";
+
+
+
 class SignupForm extends React.Component {
   createUser(e) {
     e.preventDefault();
     var self = this;
     var userData = {
       username: document.getElementById('signupUsername').value,
+      firstname: document.getElementById('signupFirstname').value,
+      secondname: document.getElementById('signupLastname').value,
       password: document.getElementById('signupPassword').value,
       email: document.getElementById('signupEmail').value
     };
+    Request
+     .post('http://localhost:3000')
+     .send(userData)
+     .end(function(err, res){
+       if (res.ok) {
+         alert('yay got ' + JSON.stringify(res.body));
+       } else {
+         alert('Oh no! error ' + res.text);
+       }
+     });
+
 
     alert("Username:\n" + document.getElementById('signupUsername').value + "\n\nPassword:\n" + document.getElementById('signupPassword').value + "\n\nEmail:\n" + document.getElementById("signupEmail").value);
   }
